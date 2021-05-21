86e7d6196b55289906ce80216879ae07c5126caa
DanielMoreti <danielvitormoreti@hotmail.com>
2021-05-20 22:25:05 -0300

    Update VerificarSaldoContaEntryPoint.java

--- banco-cap/src/main/java/br/com/bancocap/entryPoint/VerificarSaldoContaEntryPoint.java
+++ banco-cap/src/main/java/br/com/bancocap/entryPoint/VerificarSaldoContaEntryPoint.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/conta")
+@CrossOrigin(origins = "http://localhost:4200")
 public class VerificarSaldoContaEntryPoint {
 
     private VerificarSaladoContaUseCase verificarSaladoContaUseCase;
