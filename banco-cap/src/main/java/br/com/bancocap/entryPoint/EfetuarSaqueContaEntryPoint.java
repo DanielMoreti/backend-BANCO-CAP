bffaeb93d4ebad1ac6d3b5c28a76b7c13faa240f
DanielMoreti <danielvitormoreti@hotmail.com>
2021-05-20 22:25:24 -0300

    Update EfetuarSaqueContaEntryPoint.java

--- banco-cap/src/main/java/br/com/bancocap/entryPoint/EfetuarSaqueContaEntryPoint.java
+++ banco-cap/src/main/java/br/com/bancocap/entryPoint/EfetuarSaqueContaEntryPoint.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/conta")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EfetuarSaqueContaEntryPoint {
 
     private EfetuarSaqueContaUseCase efetuarSaqueContaUseCase;
